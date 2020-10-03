c7b672dbe8ed6a4950cb2b9208f2d6fed377f509
Christian Esneul <nawaplayz@gmail.com>
2020-10-03 14:36:32 -0500

    Added loop to display motor encoders
    This loop also categorizes motors depending on their 'Type'.  
    
    Might be inefficient to use a loop and StringBuilder instead of the addData function. Should probably do some testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Driving/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Driving/Manual.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.Opmodes.Driving;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.HardwareTypes.MotorTypes;
+import org.firstinspires.ftc.teamcode.HardwareTypes.Motors;
 import org.firstinspires.ftc.teamcode.Utility.Controller;
 import org.firstinspires.ftc.teamcode.Utility.RobotHardware;
 import org.firstinspires.ftc.teamcode.Utility.TelemetryTools;
@@ -27,17 +29,31 @@ public class Manual extends RobotHardware {
     @Override
     public void start() {
         super.start();
-        driveMenu.append(TelemetryTools.setHeader(2, "Drive Menu"));
     }
 
     @Override
     public void loop() {
         super.loop();
+        driveMenu.setLength(0);
+        driveMenu.append(TelemetryTools.setHeader(1, "Drive Menu"));
 
         primary.update();
         secondary.update();
 
         motorUtility.setDriveForSimpleMecanum(primary.left_stick_x, primary.left_stick_y, primary.right_stick_x, primary.right_stick_y);
+        for (MotorTypes type : MotorTypes.values()) {
+            driveMenu.append(TelemetryTools.setHeader(4, type.name())).append("\n");
+            for (Motors motor : Motors.values()) {
+                if(type != motor.getType()) continue;
+                driveMenu.append(motor.name())
+                        .append(": ")
+                        .append(motorUtility.getEncoderValue(motor))
+                        .append("\n");
+            }
+        }
+
+
+        telemetry.addLine(driveMenu.toString());
     }
 
 }
