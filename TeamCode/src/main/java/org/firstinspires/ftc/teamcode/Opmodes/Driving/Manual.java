844f6fba2617e3a79a1e4b0b23b3caad243054d5
Christian Esneul <nawaplayz@gmail.com>
2020-11-28 12:23:14 -0600

    Added IMU and MecanumNavigation to manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Driving/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Driving/Manual.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.Opmodes.Driving;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.HardwareTypes.IMU;
 import org.firstinspires.ftc.teamcode.HardwareTypes.Motors;
 import org.firstinspires.ftc.teamcode.Utility.*;
 import org.firstinspires.ftc.teamcode.Utility.Autonomous.Waypoints;
+import org.firstinspires.ftc.teamcode.Utility.Mecanum.MecanumNavigation;
+import org.firstinspires.ftc.teamcode.Utility.Odometry.IMUUtilities;
 
 import static org.firstinspires.ftc.teamcode.Opmodes.Driving.Manual.DashboardVariables.*;
 
@@ -33,14 +36,21 @@ public class Manual extends RobotHardware {
     @Override
     public void start() {
         super.start();
-        Waypoints waypoints = new Waypoints(this);
-        waypoints.test();
+        mecanumNavigation = new MecanumNavigation(this, Configuration.getDriveTrainMecanum());
+        mecanumNavigation.initialize(new MecanumNavigation.Navigation2D(0, 0, 0));
+
+        imuUtil = new IMUUtilities(this, IMU.IMU1.getName());
+        imuUtil.setCompensatedHeading(0);
     }
-    @Override public void loop() {
+
+    @Override
+    public void loop() {
         super.loop();
 
         motorUtility.setDriveForSimpleMecanum(primary.left_stick_x * drivespeed, primary.left_stick_y * drivespeed,
                 primary.right_stick_x * drivespeed, primary.right_stick_y * drivespeed);
+
+        imuUtil.update();
 
         if(primary.leftBumperOnce()) {
             drivespeed = drivespeed <= 0.0 ? 0.0 : drivespeed - 0.1;
@@ -49,9 +59,9 @@ public class Manual extends RobotHardware {
         }
 
         if(primary.dpadUpOnce()) {
-            launchspeed = launchspeed >= 1.0 ? 1.0 : launchspeed + 0.01;
+            launchspeed = launchspeed >= 1.0 ? 1.0 : launchspeed + 0.1;
         } else if(primary.dpadDownOnce()) {
-            launchspeed = launchspeed <= 0.0 ? 0.0 : launchspeed - 0.01;
+            launchspeed = launchspeed <= 0.0 ? 0.0 : launchspeed - 0.1;
         }
 
         if(primary.A()) {
@@ -73,6 +83,7 @@ public class Manual extends RobotHardware {
         telemetry.addData("Drive speed:         ", df.format(drivespeed));
         telemetry.addData("Launcher speed:      ", df.format(launchspeed));
         telemetry.addData("Loop time:           ", df_precise.format(period.getAveragePeriodSec()) + "s");
+        telemetry.addData("IMU Heading:         ", imuUtil.getCompensatedHeading());
     }
 
     int launcherTicks, launcherPreviousTicks, launcherDeltaTicks = 0;
