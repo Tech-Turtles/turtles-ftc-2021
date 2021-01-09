f78394ff78de4218cddb23be8e4208ac28a5fdeb
Christian Esneul <nawaplayz@gmail.com>
2021-01-09 15:53:34 -0600

    Added braking for motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTypes/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTypes/Motors.java
@@ -6,10 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public enum Motors {
 
-    FRONT_LEFT( "front left",   ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.REVERSE, ZeroPowerBehavior.FLOAT, RunMode.RUN_USING_ENCODER),
-    FRONT_RIGHT("front right",  ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.FORWARD, ZeroPowerBehavior.FLOAT, RunMode.RUN_USING_ENCODER),
-    BACK_LEFT(  "back left",    ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.REVERSE, ZeroPowerBehavior.FLOAT, RunMode.RUN_USING_ENCODER),
-    BACK_RIGHT( "back right",   ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.FORWARD, ZeroPowerBehavior.FLOAT, RunMode.RUN_USING_ENCODER),
+    FRONT_LEFT( "front left",   ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.REVERSE, ZeroPowerBehavior.BRAKE, RunMode.RUN_USING_ENCODER),
+    FRONT_RIGHT("front right",  ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.FORWARD, ZeroPowerBehavior.BRAKE, RunMode.RUN_USING_ENCODER),
+    BACK_LEFT(  "back left",    ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.REVERSE, ZeroPowerBehavior.BRAKE, RunMode.RUN_USING_ENCODER),
+    BACK_RIGHT( "back right",   ExpansionHubs.HUB1, MotorTypes.DRIVE, DcMotorSimple.Direction.FORWARD, ZeroPowerBehavior.BRAKE, RunMode.RUN_USING_ENCODER),
 
     INTAKE("intake", ExpansionHubs.HUB2, MotorTypes.OTHER, DcMotorSimple.Direction.FORWARD, ZeroPowerBehavior.FLOAT, RunMode.RUN_USING_ENCODER),
     LAUNCHER("launcher", ExpansionHubs.HUB2, MotorTypes.OTHER, DcMotorSimple.Direction.FORWARD, ZeroPowerBehavior.FLOAT, RunMode.RUN_USING_ENCODER);
