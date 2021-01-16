a9d26455f05885e2396ee77d7f979e5b7385429f
Ashley Trowell <ajtrowell@gmail.com>
2021-01-16 02:12:52 -0600

    Simulation tested trajectories imported
    Trajectories tested in simulation.
    Should provide common start and end points 
    to allow somewhat modular state machine 
    design.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/TrajectoryRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/TrajectoryRR.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Opmodes.Autonomous;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 
@@ -10,23 +11,34 @@ public class TrajectoryRR {
 
     public SampleMecanumDrive drive;
 
-    Pose2d START_WALL = new Pose2d(-62,-42,Math.toRadians(180));
-    Pose2d START_CENTER = new Pose2d(-62,-24,Math.toRadians(180));
-    Pose2d RINGS = new Pose2d(-24,-36,Math.toRadians(180));
-    Pose2d SHOOT = new Pose2d(-2,-36,Math.toRadians(180));
-    Pose2d ZONE_A = new Pose2d(12,-60,Math.toRadians(0)); // could be -90 so overshooting hits the wall
-    Pose2d ZONE_B = new Pose2d(-36,36,Math.toRadians(0));
-    Pose2d ZONE_C = new Pose2d(-60,60,Math.toRadians(0));
-    Pose2d PARK = new Pose2d(12,-36,Math.toRadians(180));
 
-    Pose2d TO_ZONE = new Pose2d(-24,-60,Math.toRadians(180));
-    Pose2d ZONE_VARIABLE;
+    Pose2d ringOffset = new Pose2d(2.0,5.0,0.0);
+    Pose2d wobbleOffset = new Pose2d(-12.0,0.0,0.0);
 
-    Trajectory Traj_shoot;
-    Trajectory Traj_pickup_rings;
-    Trajectory Traj_shoot2;
-    Trajectory Traj_park;
+    Pose2d START_WALL = new Pose2d(-62.0, -42.0,Math.toRadians(180.0));
+    Pose2d START_CENTER = new Pose2d(-62.0, -24.0,Math.toRadians(180.0));
+    Pose2d RINGS = new Pose2d(-24.0, -36.0,Math.toRadians(180.0)).plus(ringOffset);
+    Pose2d SHOOT = new Pose2d(-2.0, -36.0,Math.toRadians(180.0)).plus(ringOffset);
+    Pose2d ZONE_A = new Pose2d(12.0, -60.0,Math.toRadians(0.0)).plus(wobbleOffset);
+    Pose2d ZONE_B = new Pose2d(36.0, -36.0,Math.toRadians(0.0)).plus(wobbleOffset);
+    Pose2d ZONE_C = new Pose2d(60.0, -60.0,Math.toRadians(0.0)).plus(wobbleOffset);
+    Pose2d PARK = new Pose2d(12.0, -36.0,Math.toRadians(180.0)).plus(ringOffset);
 
+    // TO_ZONE renamed to WALL_WAY
+    Pose2d WALL_WAY = new Pose2d(-24.0, -56.0,Math.toRadians(180.0));
+    Pose2d WALL_WAY_START = WALL_WAY.plus(new Pose2d(-15.0,4.0,0.0));
+    // Configurables.  wobbleTangent should be 0.0 for ZONE_B, -45.0 otherwise
+    Pose2d ZONE_VARIABLE = ZONE_A;
+    Double wobbleTangent = -45.0;
+
+
+    public Trajectory trajToShoot1;
+    public Trajectory trajToPark;
+    public Trajectory trajPickupRings;
+    public Trajectory trajToShoot2;
+    public Trajectory trajStartToZone;
+    public Trajectory trajFromShootToZone;
+    public Trajectory trajZoneToShoot1;
 
 
     public TrajectoryRR(SampleMecanumDrive drive) {
@@ -43,17 +55,24 @@ public class TrajectoryRR {
         switch(zone) {
             case A:
                 ZONE_VARIABLE = ZONE_A;
+                wobbleTangent = -45.0;
                 break;
             case B:
                 ZONE_VARIABLE = ZONE_B;
+                wobbleTangent = 0.0;
                 break;
             case C:
                 ZONE_VARIABLE = ZONE_C;
+                wobbleTangent = -45.0;
                 break;
             default:
         }
 
         init();
+    }
+
+    public Vector2d toVector2d(Pose2d pose) {
+        return new Vector2d(pose.getX(),pose.getY());
     }
 
 
@@ -69,23 +88,50 @@ public class TrajectoryRR {
         D) drive, drop wobble, shoot, pickup rings, shoot 2, second wobble, park
          */
 
-        Traj_shoot= drive.trajectoryBuilder(START_WALL)
-                .splineTo(TO_ZONE)
-                //.splineTo(ZONE_VARIABLE)
-                .splineTo(SHOOT)
-                .build();
 
-        Traj_pickup_rings = drive.trajectoryBuilder(SHOOT)
-                .splineTo(RINGS)
-                .build();
+        // Start with diagonal 2.5
+        trajToShoot1 = drive.trajectoryBuilder(START_WALL)
+                        //.lineTo(toVector2d(START_WALL.plus(Pose2d(10.0,-10.0,0.0))))
+                        .lineTo(toVector2d(WALL_WAY_START))
+                        .splineToLinearHeading(WALL_WAY,Math.toRadians(0.0))
+                        .splineToLinearHeading(SHOOT,Math.toRadians(90.0))
+                        .build();
 
-        Traj_shoot2 = drive.trajectoryBuilder(RINGS)
-                .splineTo(SHOOT)
-                .build();
+        // Park immediately after shooting
+        trajToPark = drive.trajectoryBuilder(trajToShoot1.end())
+                        .splineToSplineHeading(PARK,Math.toRadians(0.0))
+                        .build();
+        //list.add(trajToPark)
 
-        Traj_park = drive.trajectoryBuilder(SHOOT)
-                .splineTo(PARK)
-                .build();
+        // From shooting position to rings pickup
+        trajPickupRings = drive.trajectoryBuilder(trajToShoot1.end())
+                        .splineToSplineHeading(RINGS,Math.toRadians(0.0))
+                        .build();
+
+        // Second batch of shooting after picking up rings
+        trajToShoot2 = drive.trajectoryBuilder(trajPickupRings.end())
+                        .splineToSplineHeading(SHOOT,Math.toRadians(0.0))
+                        .build();
+
+        // Drive from start to Zone
+        trajStartToZone = drive.trajectoryBuilder(START_WALL)
+                        //.lineTo(toVector2d(START_WALL.plus(Pose2d(10.0,-10.0,0.0))))
+                        .lineTo(toVector2d(WALL_WAY_START))
+                        .splineToLinearHeading(WALL_WAY,Math.toRadians(0.0))
+                        .splineToSplineHeading(ZONE_VARIABLE,Math.toRadians(wobbleTangent))
+                        .build();
+
+        // Drive from shoot to Zone
+        trajFromShootToZone = drive.trajectoryBuilder(SHOOT)
+                        //.splineTo(toVector2d(ZONE_VARIABLE),Math.toRadians(wobbleTangent))
+                        .lineToLinearHeading(ZONE_VARIABLE)
+                        .build();
+
+        // Drive from Zone to Shoot1
+        trajZoneToShoot1 = drive.trajectoryBuilder(trajStartToZone.end())
+                        .lineToLinearHeading(SHOOT)
+                        .build();
+
 
     }
 }
