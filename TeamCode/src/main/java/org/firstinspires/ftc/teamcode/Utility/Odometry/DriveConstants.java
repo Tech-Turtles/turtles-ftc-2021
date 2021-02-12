15ea0490a048d86a279154243223b74c84007f61
Christian Esneul <nawaplayz@gmail.com>
2021-02-12 09:52:46 -0600

    Increased MAX_ACCEL & MAX_VEL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/Odometry/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/Odometry/DriveConstants.java
@@ -63,8 +63,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 70;
-    public static double MAX_ACCEL = 30;
+    public static double MAX_VEL = 90;
+    public static double MAX_ACCEL = 40;
     public static double MAX_ANG_VEL = Math.toRadians(90);
     public static double MAX_ANG_ACCEL = Math.toRadians(45);
 
