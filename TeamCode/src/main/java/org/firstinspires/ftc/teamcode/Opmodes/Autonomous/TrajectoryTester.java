7ad47efe7e5f9549ce3e7c6d1ac66a641282531e
Christian Esneul <nawaplayz@gmail.com>
2021-04-03 17:00:02 -0500

    Trajectory test trajectory list to move through multiple hardcoded routines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/TrajectoryTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/TrajectoryTester.java
@@ -8,11 +8,15 @@ import org.firstinspires.ftc.teamcode.Utility.Autonomous.TrajectoryRR;
 import org.firstinspires.ftc.teamcode.Utility.Odometry.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.Utility.RobotHardware;
 
+import java.util.ArrayList;
+
 @Autonomous(name = "Trajectory Tester", group = "E")
 public class TrajectoryTester extends RobotHardware {
     TrajectoryRR trajectoryRR = null;
     Pose2d startPosition = new Pose2d();
     Trajectory routine;
+    ArrayList<Trajectory> trajectories = new ArrayList<>();
+    int index = 0;
 
     @Override
     public void init() {
@@ -22,18 +26,33 @@ public class TrajectoryTester extends RobotHardware {
         trajectoryRR = new TrajectoryRR(mecanumDrive);
         startPosition = trajectoryRR.getSTART_CENTER();
         mecanumDrive.setPoseEstimate(startPosition);
-        routine = trajectoryRR.getTrajCenterStartToPowershotLeft();
+        trajectories.add(0, trajectoryRR.getTrajCenterStartToHighGoal());
+        trajectories.add(1, trajectoryRR.getTrajHighGoalToRingAlign());
+        trajectories.add(2, trajectoryRR.getTrajRingAlignToRingGrab());
+        trajectories.add(3, trajectoryRR.getTrajRingGrabToShootHighGoal());
+        trajectories.add(4, trajectoryRR.getTrajHighGoalToWobbleDropoffDeep());
+        routine = trajectories.get(index);
+        index++;
     }
 
     @Override
     public void start() {
         super.start();
-        mecanumDrive.followTrajectoryAsync(routine);
     }
 
     @Override
     public void loop() {
         super.loop();
+        if(primary.AOnce()) {
+            try {
+                routine = trajectories.get(index);
+            } catch (IndexOutOfBoundsException ignore) {}
+            index++;
+        }
+
+        if(primary.BOnce() && !(index >= trajectories.size())) {
+            mecanumDrive.followTrajectoryAsync(routine);
+        }
         mecanumDrive.update(packet);
     }
 }
